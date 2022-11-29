171da5de03387df5ba44d9236edfad959d6ce7d7
Marcos <marcos1jose@live.com>
2022-11-28 22:57:20 -0300

    adicionei javascript, imagens apliando

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $(".growImage")
+    .mouseover(function () {
+      $(this)
+        .stop()
+        .animate({ width: "100%", left: "0px", top: "0px" }, 400, "swing");
+    })
+    .mouseout(function () {
+      $(this)
+        .stop()
+        .animate({ width: "80%", left: "15px", top: "15px" }, 200, "swing");
+    });
+});
